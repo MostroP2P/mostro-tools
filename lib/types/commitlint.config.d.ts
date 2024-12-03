0f2126c25a0128ffa72345fd86dda8f106297607
Andrea Diaz Correia <andrea.diaz.correia@gmail.com>
2024-12-03 15:49:34 -0300

    feat: adding a etc directory with some configs for more readability

--- lib/types/commitlint.config.d.ts
+++
@@ -1,3 +0,0 @@
-import type { UserConfig } from '@commitlint/types';
-declare const _default: UserConfig;
-export default _default;
--- lib/types/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const foo = 'bar';
-export declare function wait(timeout?: number): Promise<unknown>;
-export declare const dirname: string;
